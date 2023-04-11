2c40eb357778198b50e11f50e06c17abf92a673e
Deniz Groenhoff <deniz.groenhoff@uni-oldenburg.de>
2023-04-11 17:49:06 +0200

    feat(Natives): Added crossmap for some natives and types

--- src/GTA/NativeCallContext.cpp
+++ src/GTA/NativeCallContext.cpp
@@ -7,10 +7,10 @@ GTA::NativeCallContext::NativeCallContext() {
 VOID GTA::NativeCallContext::Reset() {
 	pReturnValue = &pReturnStack[0];
 	uDataCount = 0;
+	uParamCount = 0;
 	pParams = &pParamStack[0];
 	memset(vecsOut, 0, sizeof(vecsOut));
 	memset(vecsIn, 0, sizeof(vecsIn));
 	memset(pReturnStack, 0, sizeof(pReturnStack));
 	memset(pParamStack, 0, sizeof(pParamStack));
-	uParamCount = 0;
 }
---
+++ src/GTA/Types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace GTA {
+
+	typedef UINT32 Entity;
+	typedef UINT32 Ped;
+	typedef UINT32 Player;
+
+}
